6b6be7af2996b491bbd1c25eb745cc860863ac7c
Caleb Terrill <>
2022-01-06 17:55:28 -0800

    Detects yellow object

---
+++ videocapture.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, img = cap.read()
+    hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+
+    yellow_lower = np.array([40, 100, 100])
+    yellow_upper = np.array([100, 255, 255])
+    mask_yellow = cv2.inRange(hsv, yellow_lower, yellow_upper)
+
+    contours,hierarchy = cv2.findContours(mask_yellow, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnt = max(contours, key = cv2.contourArea)
+
+    rect = cv2.minAreaRect(cnt)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    cv2.drawContours(img,[box],0,(0,0,255),2)
+
+    cv2.imshow('frame',img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
